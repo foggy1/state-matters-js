a46b03deeda0ca916dd00f17899ecc0a5de3223b
Austin Lanari <austin.lanari@gmail.com>
2017-01-21 11:51:46 -0500

    implement yarn packaging

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ var config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
 
         query: {
           presets: ['es2015', 'react']
